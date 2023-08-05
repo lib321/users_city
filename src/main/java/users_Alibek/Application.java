602e77fec285ab6e5e4d4a1db46a61cfd4c44e22
baimuhtar <al16ek199@gmail.com>
2023-08-05 15:25:48 +0600

    first TypedQuery

--- src/main/java/users_Alibek/Application.java
+++ src/main/java/users_Alibek/Application.java
@@ -3,6 +3,8 @@ package users_Alibek;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.EntityManagerFactory;
 import jakarta.persistence.Persistence;
+import jakarta.persistence.TypedQuery;
+import users_Alibek.entity.Users;
 
 import java.util.Scanner;
 
@@ -15,14 +17,26 @@ public class Application {
 
         System.out.println("- Авторизация [1]");
         System.out.println("- Регистрация [2]");
-        System.out.print("Выбреите действие: ");
+        System.out.print("Выберите действие: ");
         Scanner scanner = new Scanner(System.in);
         String str = scanner.nextLine();
 
         try {
             manager.getTransaction().begin();
             switch (str) {
+                case "1" -> {
+                    System.out.print("Введите логин: ");
+                    String login = scanner.nextLine();
+                    TypedQuery<Users> usersTypedQuery = manager.createQuery("select u from Users u where u.login = ?1",
+                            Users.class);
+                    usersTypedQuery.setParameter(1, login);
+                    Users user = usersTypedQuery.getSingleResult();
+                    if (user != null) {
 
+                    } else {
+                        System.out.println("Неверный логин");
+                    }
+                }
             }
         } catch (Exception e) {
             manager.getTransaction().rollback();
