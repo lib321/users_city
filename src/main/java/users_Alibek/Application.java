367e4c72564203fb2b26a0314311dc88c2971df7
alibek <al16ek199@gmail.com>
2023-08-05 22:39:20 +0600

    started the registration logic

--- src/main/java/users_Alibek/Application.java
+++ src/main/java/users_Alibek/Application.java
@@ -4,8 +4,10 @@ import jakarta.persistence.EntityManager;
 import jakarta.persistence.EntityManagerFactory;
 import jakarta.persistence.Persistence;
 import jakarta.persistence.TypedQuery;
+import users_Alibek.entity.City;
 import users_Alibek.entity.Users;
 
+import java.util.List;
 import java.util.Scanner;
 
 public class Application {
@@ -23,17 +25,19 @@ public class Application {
 
         try {
             manager.getTransaction().begin();
+            String login;
+            String password;
             switch (str) {
                 case "1" -> {
                     System.out.print("Введите логин: ");
-                    String login = scanner.nextLine();
+                    login = scanner.nextLine();
                     TypedQuery<Users> usersTypedQuery = manager.createQuery("select u from Users u where u.login = ?1",
                             Users.class);
                     usersTypedQuery.setParameter(1, login);
                     Users user = usersTypedQuery.getSingleResult();
                     if (user != null) {
                         System.out.print("Введите пароль: ");
-                        String password = scanner.nextLine();
+                        password = scanner.nextLine();
                         boolean correctPassword = false;
                         while (!correctPassword) {
                             if (password.equals(user.getPassword())) {
@@ -50,7 +54,21 @@ public class Application {
                         System.out.println("Неверный логин");
                     }
                 }
+                case "2" -> {
+                    System.out.print("Введите логин: ");
+                    Users user = new Users();
+                    login = scanner.nextLine();
+                    System.out.print("Введите пароль: ");
+                    password = scanner.nextLine();
+                    TypedQuery<City> cityTypedQuery = manager.createQuery("select c from City c", City.class);
+                    System.out.print("Введите город: ");
+                    List<City> cities = cityTypedQuery.getResultList();
+                    for (City city : cities) {
+                        System.out.println(city.getName());
+                    }
+                }
             }
+            manager.getTransaction().commit();
         } catch (Exception e) {
             manager.getTransaction().rollback();
             e.printStackTrace();
