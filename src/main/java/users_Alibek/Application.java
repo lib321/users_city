df86e9288375c104deeff0c6c4d8997ad2bae3bf
baimuhtar <al16ek199@gmail.com>
2023-08-05 15:18:29 +0600

    started writing logic in application

--- src/main/java/users_Alibek/Application.java
+++ src/main/java/users_Alibek/Application.java
@@ -4,6 +4,8 @@ import jakarta.persistence.EntityManager;
 import jakarta.persistence.EntityManagerFactory;
 import jakarta.persistence.Persistence;
 
+import java.util.Scanner;
+
 public class Application {
 
     public static void main(String[] args) {
@@ -11,5 +13,20 @@ public class Application {
         EntityManagerFactory factory = Persistence.createEntityManagerFactory("main");
         EntityManager manager = factory.createEntityManager();
 
+        System.out.println("- Авторизация [1]");
+        System.out.println("- Регистрация [2]");
+        System.out.print("Выбреите действие: ");
+        Scanner scanner = new Scanner(System.in);
+        String str = scanner.nextLine();
+
+        try {
+            manager.getTransaction().begin();
+            switch (str) {
+
+            }
+        } catch (Exception e) {
+            manager.getTransaction().rollback();
+            e.printStackTrace();
+        }
     }
 }
